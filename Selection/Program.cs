8b1719c5c0b21dd6c452cf2019e5ba7090d89442
fulfordcs <fulfordcs@gmail.com>
2024-09-13 09:58:14 +0100

    Add project files.

---
+++ Selection/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("What day is it?");
+string day = Console.ReadLine();
+
+if (day.ToUpper().Trim() == "MONDAY")
+{
+    Console.WriteLine("Oh dear, 4 days 'til the weekend");
+} else if (day.ToUpper().Trim() == "TUESDAY")
+{
+    Console.WriteLine("At least it's not Monday");
+} else if (day.ToUpper().Trim() == "WEDNESDAY")
+{
+    Console.WriteLine("You're nearly half way there");
+}
+else if (day.ToUpper().Trim() == "THURSDAY")
+{
+    Console.WriteLine("It's Friday tomorrow!");
+}
+else if (day.ToUpper().Trim() == "FRIDAY")
+{
+    Console.WriteLine("Oh well, it's not long til the weekend");
+}
