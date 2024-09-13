a00f68c4eec7a7a528c0882c73feb8f5bc993e5f
fulfordcs <fulfordcs@gmail.com>
2024-09-13 10:05:50 +0100

    Changed if to switch

--- Selection/Program.cs
+++ Selection/Program.cs
@@ -1,6 +1,33 @@
 ﻿Console.Write("What day is it?");
 string day = Console.ReadLine();
 
+switch(day.Trim().ToLower())
+{
+    case "monday":
+        Console.WriteLine("Oh dear, 4 days 'til the weekend");
+        break;
+    case "tuesday":
+        Console.WriteLine("At least it's not Monday");
+        break;
+    case "wednesday":
+        Console.WriteLine("At least it's not Monday");
+        break;
+    case "thursday":
+        Console.WriteLine("At least it's not Monday");
+        break;
+    case "friday":
+        Console.WriteLine("At least it's not Monday");
+        break;
+    case "saturday":
+    case "sunday":
+        Console.WriteLine("At least it's not Monday");
+        break;
+    default:
+        Console.WriteLine("I don't recognise that, which means it's probably Monday?");
+        break;
+}
+
+/*
 if (day.ToUpper().Trim() == "MONDAY")
 {
     Console.WriteLine("Oh dear, 4 days 'til the weekend");
@@ -19,3 +46,4 @@ else if (day.ToUpper().Trim() == "FRIDAY")
 {
     Console.WriteLine("Oh well, it's not long til the weekend");
 }
+*/
